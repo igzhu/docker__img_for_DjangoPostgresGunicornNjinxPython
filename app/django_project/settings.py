7e245dade66a6151fcb4ce6e960056cf5da86082
igzhu <z.89@aim.com>
2023-05-28 20:39:11 +0300

    postgres, njinx

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^e)b)#4nht+cn@-!9&ofijy3vv@=wku$duga^gf(tc#%2*r=u1'
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^e)b)#4nht+cn@-!9&ofijy3vv@=wku$duga^gf(tc#%2*r=u1'
+#SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = int(environ.get('DEBUG', default=0))
+DEBUG = True
+#DEBUG = int(environ.get('DEBUG', default=0))
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(' ')
 
 
 
